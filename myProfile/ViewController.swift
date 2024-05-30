7ed0d840a050cac86047e66d6c7f0f9fa67ad4be
chomi <hhww0328@gmail.com>
2024-05-30 15:14:57 +0900

    new images ontents

--- myProfile/ViewController.swift
+++ myProfile/ViewController.swift
@@ -13,22 +13,26 @@ class ViewController: UIViewController{
         @IBOutlet weak var myFirstText: UILabel!
     
         
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            // Do any additional setup after loading the view.
-            mySelfie.image = UIImage(named: "selfie")
-            
-            let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
-            backgroundImage.image = UIImage(named: "AFE40E13-EE0D-4753-B61C-2E6B20A8B663_1_105_c")
-            backgroundImage.contentMode = .scaleAspectFill
-            backgroundImage.alpha = 0.2
-            
-            self.view?.insertSubview(backgroundImage, at: 0)
-            
-            
-            
-            myFirstText.text = " "
-        }
+        
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        mySelfie.image = UIImage(named: "selfie")
+        
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "AFE40E13-EE0D-4753-B61C-2E6B20A8B663_1_105_c")
+        backgroundImage.contentMode = .scaleAspectFill
+        backgroundImage.alpha = 0.2
+        
+        self.view?.insertSubview(backgroundImage, at: 0)
+        
+        myFirstText.text = " "
+        
+        
+        
+       
+    }
 }
 
 
--- myProfile/mbtiViewController.swift
+++ myProfile/mbtiViewController.swift
@@ -9,21 +9,28 @@ import UIKit
 
 class mbtiViewController: UIViewController {
 
+    @IBOutlet weak var inpfImages: UIImageView!
+    
+    
+    var images: [UIImage] = [] //변수에 배열 선언
+    var count = 0
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        images = [
+            UIImage(named: "infp01")!,
+            UIImage(named: "infp02")!,
+            UIImage(named: "infp03")!
+        ]
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func didMyTapBtn(_ sender: Any) {
+        inpfImages.image = images[count]
+        count = count + 1
     }
-    */
-
+    
 }
