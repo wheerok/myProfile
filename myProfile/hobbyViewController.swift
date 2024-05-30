9510639f05ec1f739fb59691511da606dd238e18
chomi <hhww0328@gmail.com>
2024-05-30 16:01:08 +0900

    새로운 페이지 추가 -> 취미

---
+++ myProfile/hobbyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  hobbyViewController.swift
+//  myProfile
+//
+//  Created by t2023-m0113 on 5/30/24.
+//
+
+import UIKit
+
+class hobbyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
