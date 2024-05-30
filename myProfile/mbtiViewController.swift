08be24aa4d0644298faf3985b7da7f4bfc860f79
chomi <hhww0328@gmail.com>
2024-05-30 14:37:02 +0900

    new chomi mbti

---
+++ myProfile/mbtiViewController.swift
@@ -0,0 +1,29 @@
+//
+//  mbtiViewController.swift
+//  myProfile
+//
+//  Created by t2023-m0113 on 5/30/24.
+//
+
+import UIKit
+
+class mbtiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
